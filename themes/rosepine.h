8cd7be38b7a5268052cfbb097f3bacb2505f474c
Kavi Arya <kaviarya047@tutamail.com>
2025-01-02 19:30:22 +0530

    First

---
+++ themes/rosepine.h
@@ -0,0 +1,6 @@
+static const char col_1[]       = "#26233a";
+static const char col_2[]       = "#26233a";
+static const char col_3[]       = "#e0def4";
+static const char col_4[]       = "#c4a7e7";
+static const char col_5[]       = "#26233a";
+static const char col_6[]       = "#26233a";
